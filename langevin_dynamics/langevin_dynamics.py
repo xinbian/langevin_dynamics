c562c4367a30010d54c5250640062222217b6772
Xin Bian <bianxin2006@gmail.com>
2016-09-16 13:45:57 -0400

    test travis #1

--- langevin_dynamics/langevin_dynamics.py
+++ langevin_dynamics/langevin_dynamics.py
@@ -1,1 +1,4 @@
-# -*- coding: utf-8 -*-
+a = 37
+# simple if statement
+if a == 37:  # two equals signs for comparison (one for assignment)
+    print('yes, a is equal to 37')# -*- coding: utf-8 -*-
