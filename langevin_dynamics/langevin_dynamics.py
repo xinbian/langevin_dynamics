0d75c342aa246715f57577d67959375f467bedae
Xin Bian <bianxin2006@gmail.com>
2016-09-22 19:00:38 -0400

    direc

--- langevin_dynamics/langevin_dynamics.py
+++ langevin_dynamics/langevin_dynamics.py
@@ -2,13 +2,13 @@ import unittest
 import numpy as np
 import matplotlib.pyplot as plt
 
-f = open('./potential.d','w')
+f = open('/home/travis/build/xinbian/langevin_dynamics/langevin_dynamics/potential.d','w')
 for i in range(1, 201, 1):
   f.write("%10s  %10s  %10s\n" % (i, np.sin(i), np.cos(i)))
 f.close()
 
 #read inout parameter
-with open('./input.d', 'r') as f:
+with open('/home/travis/build/xinbian/langevin_dynamics/langevin_dynamics/input.d', 'r') as f:
        x=f.readline()
        v=f.readline()
        temp=f.readline()
@@ -33,7 +33,7 @@ eta=np.random.normal(mu,sigma,nstep)
 #count, bins, ignored = plt.hist(eta, 30, normed=True)
 
 
-f = open('./output.d','w')
+f = open('/home/travis/build/xinbian/langevin_dynamics/langevin_dynamics/output.d','w')
 for i in range(1, nstep+1):  
     a=(-dampcoeff*v+eta[i-1]-np.cos(x))/m
     v=v+deltat*a
