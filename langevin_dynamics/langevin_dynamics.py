3ec8100df91132ff083d073e9d92e2ebcb0abf5b
Xin Bian <bianxin2006@gmail.com>
2016-09-22 17:03:05 -0400

    potential energy not specified

--- langevin_dynamics/langevin_dynamics.py
+++ langevin_dynamics/langevin_dynamics.py
@@ -1,4 +1,44 @@
-a = 37
-# simple if statement
-if a == 37:  # two equals signs for comparison (one for assignment)
-    print('yes, a is equal to 37')# -*- coding: utf-8 -*-
+import unittest
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = open('./potential.d','w')
+for i in range(1, 201, 1):
+  f.write("%10s  %10s  %10s\n" % (i, np.sin(i), np.cos(i)))
+f.close()
+
+#read inout parameter
+with open('./input.d', 'r') as f:
+       x=f.readline()
+       v=f.readline()
+       temp=f.readline()
+       dampcoeff=f.readline()
+       deltat=f.readline()
+       totalt=f.readline()
+       m=f.readline()
+       
+x=float(x)
+v=float(v)
+temp=float(temp)
+dampcoeff=float(dampcoeff)
+deltat=float(deltat)
+totalt=float(totalt)
+m=float(m)
+time=0
+nstep=int(totalt/deltat)
+
+#generate random number
+mu, sigma=0, 2*np.sqrt(dampcoeff)*temp
+eta=np.random.normal(mu,sigma,nstep)
+#count, bins, ignored = plt.hist(eta, 30, normed=True)
+
+
+f = open('./output.d','w')
+for i in range(1, nstep+1):  
+    a=(-dampcoeff*v+eta[i-1]-np.cos(x))/m
+    v=v+deltat*a
+    x=x+v*deltat
+    time=time+deltat
+    f.write("%3s %5s %10s %10s\n" % (i, time, x, v))
+    i=i+1
+f.close()
