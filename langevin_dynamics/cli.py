e54f85d5ebb5bbdc8eb7e62187c8c339c862c63e
Xin Bian <bianxin2006@gmail.com>
2016-09-28 11:12:55 -0400

    cli

--- langevin_dynamics/cli.py
+++ langevin_dynamics/cli.py
@@ -4,11 +4,11 @@ import click
 
 @click.command()
 def main(args=None):
-    """Console script for langevin_dynamics"""
-    click.echo("Replace this message by putting your code into "
-                "langevin_dynamics.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+#    """Console script for langevin_dynamics"""
+#    click.echo("Replace this message by putting your code into "
+#                "langevin_dynamics.cli.main")
+#    click.echo("See click documentation at http://click.pocoo.org/")
 
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
